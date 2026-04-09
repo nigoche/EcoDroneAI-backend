e6d68eb023da5a0c0c604cbddbf9852f0828a343
Jesús García Nigoche <jgarcianigoche@gmail.com>
2026-04-09 12:24:35 -0500

    Creación del modelo venta

--- app/db/models.py
+++ app/db/models.py
@@ -1,10 +1,14 @@
 from app.db.database import Base
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from datetime import datetime
+from sqlalchemy.schema import ForeignKey
+from sqlalchemy.orm import relationship
 
 class Usuario(Base):
     __tablename__ = "usuario"
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
+    usuario = Column(String(15), nullable=False)
+    clave = Column(String(15), nullable=False)
     nombre = Column(String(15), nullable=False)
     apellido = Column(String(15), nullable=False)
     direccion = Column(String(255), nullable=False)
@@ -12,3 +16,11 @@ class Usuario(Base):
     correo = Column(String(127), nullable=False, unique=True)
     antiguedad = Column(DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
     estado = Column(Boolean, nullable=False, default=True)
+    venta = relationship("Venta", backref="usuario", cascade="delete, merge")
+
+class Venta(Base):
+    __tablename__ = "venta"
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
+    id_usuario = Column(Integer, ForeignKey('usuario.id', ondelete='CASCADE'), nullable=False)
+    venta = Column(Integer, nullable=False)
+    ventas_productos = Column(Integer, nullable=False)

29c976660e3dc55352d1804bb81566e8dbfa7ffd
Jesús García Nigoche <jgarcianigoche@gmail.com>
2026-04-29 17:09:01 -0600

    Cadenas más largas para nombre y usuario en la base de datos

--- app/db/models.py
+++ app/db/models.py
@@ -9,8 +9,8 @@ class Usuario(Base):
     """Modelo ORM para la tabla 'usuario' en PostgreSQL."""
     __tablename__ = "usuario"
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
-    username = Column(String(15), nullable=False, unique=True)
+    username = Column(String(30), nullable=False, unique=True)
     password = Column(String(127), nullable=False)
-    nombre = Column(String(15), nullable=False)
+    nombre = Column(String(30), nullable=False)
     telefono = Column(String(10), nullable=False)
     correo = Column(String(127), nullable=False, unique=True)

f829496bccc8173f1f3b5921be5305b0f6e7b213
Jesús García Nigoche <jgarcianigoche@gmail.com>
2026-05-12 22:07:10 -0600

    Se añadió soporte de CORS para recibir peticiones de múltiples orígenes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ Punto de entrada principal de la aplicación FastAPI.
 Configura la conexión a la base de datos y registra los enrutadores.
 """
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from app.routers import user, auth
 from app.db.database import Base, engine
@@ -17,5 +18,15 @@ app = FastAPI(title="EcoDrone AI API", description="API para el manejo de usuari
 app.include_router(user.router)
 app.include_router(auth.router)
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # * para todo
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
